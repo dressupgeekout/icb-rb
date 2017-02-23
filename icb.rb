3732f3198226b5ba9bb166b8cf5dffd576da8d78
Charlotte Koch <cfkoch@edgebsd.org>
2017-02-22 22:48:38 -0800

    Update copyright.

--- icb.rb
+++ icb.rb
@@ -2,7 +2,7 @@
 # Charlotte's ICB Client Library for Ruby
 # =======================================
 #
-# Copyright 2016 Charlotte Koch <cfkoch@edgebsd.org>
+# Copyright 2016, 2017 Charlotte Koch <cfkoch@edgebsd.org>
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
